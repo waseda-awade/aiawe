42973ce241e4ac08ea8397772ff16ca7d8ad94ce
Peihao Chen <peihao.chen.jp@gmail.com>
2025-01-30 20:57:39 +0900

    Update the domain name.

--- awe_system_ui/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ awe_system_ui/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -40,7 +40,7 @@ def update_site_forward(apps, schema_editor):
     _update_or_create_site_with_sequence(
         Site,
         schema_editor.connection,
-        "app.gec.awade.waseda.ac.jp",
+        "app.awade.gec.waseda.ac.jp",
         "AWE",
     )
 
