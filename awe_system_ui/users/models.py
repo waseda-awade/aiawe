58220fa0f88145699a3cfa4643e5efc4c5f5f9b7
Peihao Chen <peihao.chen.jp@gmail.com>
2025-02-08 10:47:47 +0900

    Enhance User model with email synchronization for AllAuth
    - Override `save()` method to sync email with EmailAddress model
    - Automatically update or create primary email address when changed
    - Ensure email consistency between User and AllAuth EmailAddress
    - Add required fields to User model

--- awe_system_ui/users/models.py
+++ awe_system_ui/users/models.py
@@ -1,6 +1,10 @@
+import contextlib
+
+from allauth.account.models import EmailAddress
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager
 from django.db import models
+from django.db import transaction
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -91,6 +95,8 @@ class User(AbstractUser):
     first_name = None  # type: ignore[assignment]
     last_name = None  # type: ignore[assignment]
 
+    required_fields = ["email", "name", "username"]
+
     def __str__(self):
         return f"{self.name} <{self.email}>"
 
@@ -107,3 +113,30 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+    def save(self, *args, **kwargs):
+        """Override save to sync email with EmailAddress."""
+        email_changed = False
+        if self.pk:  # If this is an existing user
+            with contextlib.suppress(User.DoesNotExist):
+                old_user = User.objects.get(pk=self.pk)
+                if old_user.email != self.email:
+                    email_changed = True
+
+        with transaction.atomic():
+            super().save(*args, **kwargs)
+
+            if email_changed:
+                # Update or create primary email address
+                EmailAddress.objects.update_or_create(
+                    user=self,
+                    primary=True,
+                    defaults={
+                        "email": self.email,
+                        "verified": True,  # Set new email to verified
+                    },
+                )
+                # Delete any other email addresses for this user
+                EmailAddress.objects.filter(user=self).exclude(
+                    email=self.email,
+                ).delete()
