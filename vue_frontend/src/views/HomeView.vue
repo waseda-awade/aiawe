4b00fa662218e1f2b6cc9fcf75e59fb809a0de3c
Peihao Chen <peihao.chen.jp@gmail.com>
2025-02-10 22:53:35 +0900

    Add project description in home page.

--- vue_frontend/src/views/HomeView.vue
+++ vue_frontend/src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container mx-auto text-center mt-20">
     <h1 class="text-4xl font-bold mb-4">Welcome to AiAWE</h1>
-    <p class="text-lg mb-6">This is a description of the AiAWE app.</p>
+    <div class="text-lg mb-6 max-w-400 mx-auto">
+      <p>AiAWE is a project led by John M. Gayed and Judy Wang at Waseda University that evaluates student writing using Large Language Models (LLMs).</p>
+      <p>Please go to the <router-link class="text-gray-500 hover:cursor-pointer" :to="{name: 'home'}">About</router-link> page to learn more about our research.</p>
+    </div>
     <router-link :to="{name: 'login'}"><Button>Get Started</Button></router-link>
   </div>
 </template>
