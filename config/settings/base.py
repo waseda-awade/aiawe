9e109511f1eee3a6bc6de60f855371ed3c45bdb2
Peihao Chen <peihao.chen.jp@gmail.com>
2025-02-14 09:26:18 +0900

    Increase batch processing time out to 12 hours.

--- config/settings/base.py
+++ config/settings/base.py
@@ -306,10 +306,10 @@ CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-time-limit
 # TODO: set to whatever value is adequate in your circumstances
-CELERY_TASK_TIME_LIMIT = 40 * 60  # 40 minutes
+CELERY_TASK_TIME_LIMIT = 12 * 60 * 60  # 12 hours
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-soft-time-limit
 # TODO: set to whatever value is adequate in your circumstances
-CELERY_TASK_SOFT_TIME_LIMIT = 30 * 60  # 30 minutes
+CELERY_TASK_SOFT_TIME_LIMIT = 12 * 60 * 60  # 12 hours
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#beat-scheduler
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#worker-send-task-events
