26fa77fc228dcf9ad9ce19b3dfb85896ada52bbb
appliedg <69959269+appliedg@users.noreply.github.com>
2025-08-27 15:19:17 +0900

    Update HomeView.vue

--- vue_frontend/src/views/HomeView.vue
+++ vue_frontend/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto text-center md:mt-20">
     <h1 class="text-4xl font-bold mb-4">Welcome to AiAWE</h1>
     <div class="text-lg mb-6">
-      <p>AiAWE is a project led by John M. Gayed and Judy Wang at Waseda University that evaluates student writing using Large Language Models (LLMs).</p>
+      <p>AiAWE is a project led by John M. Gayed at Waseda University that evaluates student writing using Large Language Models (LLMs).</p>
       <p>Please go to the <router-link class="text-primary hover:underline" :to="{name: 'about'}">About</router-link> page to learn more about our research.<br /> Click the "Get Started" button below to register and use the app. Below is a screenshot of the app's output. Enjoy!</p>
     </div>
     <router-link :to="{name: 'login'}"><Button>Get Started</Button></router-link>
